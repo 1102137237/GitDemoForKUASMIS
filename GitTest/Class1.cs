e294e7a9ac8d63bbc9c5ca03f459a202ee195e30
1102137237 <1102137237@gm.kuas.edu.tw>
2016-03-03 06:59:26 +0800

    add class1

---
+++ GitTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitTest
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hi");
+            System.Console.ReadLine();
+        }
+    }
+}
